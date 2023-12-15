3c94931b29a864c6bd4a43d6a0b237a728892893
dackae <rosalunaicastel@gmail.com>
2023-12-15 10:45:20 +0100

    He creat el fitxer,afegit les pre i postcondicions i creat unes constants

---
+++ Estructura_i_tractament.py
@@ -0,0 +1,6 @@
+#Precondició: L'usuari té l'informació necessaria per omplir els requeriments el programa
+#constants
+alumnes="Indica la quantitat d'alumnes que vols registrar"
+dades="Introdueix les seguents dades de l'alumne separades per espais: Nom, Cognom, Edat, Nota1, Nota2, Nota3."
+#endconstants
+#Postcondició: El programa retorna dues llistes amb els alumnes segons quina condició compleixen
