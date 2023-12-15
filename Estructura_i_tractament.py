3b6e652eaa1343272e07025e65312d23df72bc92
dackae <rosalunaicastel@gmail.com>
2023-12-15 10:51:49 +0100

    Fitxer:Estructura_i_tractament.py -- He afegit una tupla i creat un bucle per demanar al usuari la cuantitat d'alumnes i dins del bucle demanar les dades de cada alumne en cada iteració.

--- Estructura_i_tractament.py
+++ Estructura_i_tractament.py
@@ -3,4 +3,9 @@
 alumnes="Indica la quantitat d'alumnes que vols registrar"
 dades="Introdueix les seguents dades de l'alumne separades per espais: Nom, Cognom, Edat, Nota1, Nota2, Nota3."
 #endconstants
+#var
+tupla=()
+#endvar
+for i in range(input(alumnes)):
+    tupla(i)=(input(dades))
 #Postcondició: El programa retorna dues llistes amb els alumnes segons quina condició compleixen
