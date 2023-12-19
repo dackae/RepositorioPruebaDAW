b479fe2c819e89f2e89d5208a64bf5d76db3615a
dackae <rosalunaicastel@gmail.com>
2023-12-19 12:43:22 +0100

    He limpiado y ordenado un poco algunas cosas del codigo, he creado la condición para la otra lista y la operacion de la media, y estoy tratando de hacer funcionar los prints

--- Estructura_i_tractament.py
+++ Estructura_i_tractament.py
@@ -2,32 +2,40 @@
 #constants
 alumnes="Indica la quantitat d'alumnes que vols registrar "
 dades="Introdueix les seguents dades de l'alumne separades per espais: Nom, Cognom, Edat, Nota1, Nota2, Nota3: "
+titol_llista2="1 o més notes majors que 8"
+titol_llista1="Mitjana superior a 7"
 #endconstants
 #var
-i=0
+suma=0
+n=0
 lista1=[]
 lista2=[]
 x=int(input(alumnes))
-a="tal tal tal 1 2 6"
-text=a.split(" ")
 #endvar
-while i < x:
+for i in range(0,x):
     dades_alumne=input(dades)
     dades_alumne=dades_alumne.split(" ")
     notas=[int(dades_alumne[3])]
     notas.append(int(dades_alumne[4]))
     notas.append(int(dades_alumne[5]))
-    print(notas)
-    tupla=(dades_alumne, notas)
+    tupla=(dades_alumne)
+    for j in range(0,3):
+        suma=suma+notas[j]
+    nota_media=suma/3
+    if nota_media>7:
+        lista1.append(tupla)
     if notas[0]>8:
-        lista1.append(tupla)
-        print("bien")
+        lista2.append(tupla)
     if notas[1]>8:
-        lista1.append(tupla)
-        print("bien")
+        lista2.append(tupla)
     if notas[2]>8:
-        lista1.append(tupla)
-        print("bien")
-    i+=1
-
+        lista2.append(tupla)
+if len(lista1)>0:
+    print(titol_llista1)
+else:
+    print("Ningun alumne ha obtingut una mitjana superior a 7")
+do {
+    print(lista1[i])
+}
+while i<len(lista1)-1:
 #Postcondició: El programa retorna dues llistes amb els alumnes segons quina condició compleixen
