0b0ee01a08b3eb7a8a70c482e15a2aca77b2230d
dackae <rosalunaicastel@gmail.com>
2023-12-19 14:31:00 +0100

    Esto es solo para guardar la idea que tengo

--- repeticio_i_cadenes.py
+++ repeticio_i_cadenes.py
@@ -1,23 +1,29 @@
 #Pre:
-text=input("Introdueix el text")
-text.casefold()
-text.split(" ")
-text_aux=text
-dic={}
-cont=0
-i=0
-while condicion_texto==False:
-    cont=0
-    salida=False
-    while salida==False:
-        if text.find(text[i])==-1:
-            salida=True
-        else:
-            cont+=1
-            del text[text.find(text[i])]
-    dic[text[i]:cont]
-    del text[i]
-    i+=1
-    if i==len(text):
-        condicion_texto=True
+#text=input("Introdueix el text")
+#text_aux=text.casefold()
+#text_aux=text_aux.split(" ")
+#dic={}
+#cont=0
+#i=0
+#condicion_texto=False
+#while condicion_texto==False:
+#    cont=0
+#    salida=False
+#    while salida==False: #por lo visto no se puede eliminar un caracter ni grupo de caracteres de un str
+#        if text.find(text_aux[i])==-1:
+     #       salida=True
+    #    else:
+   #         text_aux.find(text_aux)
+  #          cont+=1
+ #           text.split(" ")
+#            del text[text_aux(text_aux[i])]
+#    dic[text[i]:cont]
+#    del text[i]
+#    i+=1
+#    if i==len(text):
+#        condicion_texto=True
+
+
+#Lo que debo hacer seria mas o menos el comparar indice i i indice j si son iguales aumento en 1 el contador y a la vez borro el indice actual, nota, hacerlo en un while para no aumentar la j en caso de que borre el j
+#Lo del diccionario esta bien
 #Post:a
