b60f2f1b9a63837725512681ff6ad52d7a2049c9
dackae <rosalunaicastel@gmail.com>
2024-01-07 19:09:41 +0100

    He cambiado el randomizador del albert porque no funcionaba, o no lo sabia hacer funcionar yo

--- preguntes_i_respostes.py
+++ preguntes_i_respostes.py
@@ -1,9 +1,7 @@
 #PRE:
 #Juego de preguntas (en catalan))
+import random
 dic_capitales={'Bosnia y Hezergovina' : 'Sarajevo', 'España' : 'Madrid', 'Francia' : 'Paris', 'Alemania' : 'Berlín', 'Reino Unido' : 'Londres', 'Argelia' : 'Argel', 'Kenia' : 'Nairobi', 'Madagascar' : 'Antananarivo', 'India' :'Nueva Deli'}
-seed= 2
-seed= (seed*997) % 1000
-random= (seed*503) % 1000 / 100
-numero_pregunta= int(random * (len(dic_capitales)))
 lista_claves=list(dic_capitales.keys())
-print(lista_claves[numero_pregunta])
+pais_pregunta= random.choice(lista_claves)
+respuesta=str(INPUT(print('Dime la capital de', pais_pregunta)))
