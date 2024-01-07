6fc196798a02db6ead3522bf730a03e4fc40ad53
dackae <rosalunaicastel@gmail.com>
2024-01-07 19:41:31 +0100

    He añadido el sistema de puntuacion, y he puesto las condiciones para continuar y para puntuar todo con lower()

--- preguntes_i_respostes.py
+++ preguntes_i_respostes.py
@@ -1,7 +1,16 @@
 #PRE:
 #Juego de preguntas (en catalan))
 import random
+continuar=True
 dic_capitales={'Bosnia y Hezergovina' : 'Sarajevo', 'España' : 'Madrid', 'Francia' : 'Paris', 'Alemania' : 'Berlín', 'Reino Unido' : 'Londres', 'Argelia' : 'Argel', 'Kenia' : 'Nairobi', 'Madagascar' : 'Antananarivo', 'India' :'Nueva Deli'}
 lista_claves=list(dic_capitales.keys())
-pais_pregunta= random.choice(lista_claves)
-respuesta=str(INPUT(print('Dime la capital de', pais_pregunta)))
+print("Si quieres dejar de responder preguntas, di 'stop'")
+puntuacion=0
+while continuar==True:
+    pais_pregunta= random.choice(lista_claves)
+    respuesta=str(input('Dime la capital de '+pais_pregunta+': '))
+    if respuesta.lower()== 'stop':
+        continuar=False
+    if respuesta.lower() == dic_capitales[pais_pregunta].lower():
+        puntuacion+=1
+print('Esta es tu puntuación: ', puntuacion)
